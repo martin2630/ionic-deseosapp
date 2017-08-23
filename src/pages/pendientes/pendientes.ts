c1d43f4fa3ea14cdfb54986254ef2f7f50e7c4da
martin castillo <martin@punkmkt.com>
2017-08-23 04:54:37 -0500

    first commit

---
+++ src/pages/pendientes/pendientes.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-pendientes',
+  templateUrl: 'pendientes.html'
+})
+export class PendientesPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
